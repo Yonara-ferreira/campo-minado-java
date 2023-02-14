384f246f5f45ea7c7a0bc41b036160a3c84330a0
yonara ferreira <yonara.fd@gmail.com>
2023-02-13 23:47:51 -0300

    Finalização da configuração do JUnit - testes na aplicação

---
+++ src/br/com/narah/cm/modelo/Tabuleiro.java
@@ -0,0 +1,5 @@
+package br.com.narah.cm.modelo;
+
+public class Tabuleiro {
+
+}
--- teste/br/com/narah/cm/teste.java
+++ teste/br/com/narah/cm/teste.java
@@ -4,11 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class teste {
+class Teste {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testarSeIgualADois() {
+		int a = 1 + 1;
+		
+		assertEquals(2, a);
 	}
 
 }
